8df7b0b92dfb35654b527847e52f8a024bb7b35d
Malind Kowalski <malind.nikulski@gmail.com>
2018-12-03 09:18:31 -0700

    redoing codealong with ajax and callbacks

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+//Should wait for page to load before runnng AJAX request
 $(document).ready(function() {
+  //Now begin AJAX GET request, 1st param is URL w/ data
+  //2nd param is a function handling the response
   $.get('sentence.html', function(response) {
+    //Get the element on the page with id of sentences
+    //And insert the response
     $('#sentences').html(response)
-  })
-  $.get("this_doesnt_exist.html", function(data) {
-  // This will not be called because the .html file request doesn't exist
-    doSomethingGood();
-  }).fail(function(error) {
-    // This is called when an error occurs
-    console.log('Something went wrong: ' + error);
   })
 })
